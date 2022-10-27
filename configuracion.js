ce4c66ce249eb72e03a3ff8651937554d38976cc
Helton J. Lopez <heltonjlopez@gmail.com>
2022-10-26 20:36:56 -0600

    Update

---
+++ configuracion.js
@@ -0,0 +1,17 @@
+/*
+const configuraciones = () => {
+    return {
+        indiceDescanso: 1.5,
+        indiceCansancio: 0.7
+    }
+}
+*/
+
+function configuraciones(){
+    return {
+        indiceDescanso: 1.5,
+        indiceCansancio: 0.7
+    }
+}
+
+module.exports = configuraciones()
--- index.js
+++ index.js
@@ -1,19 +1,21 @@
+var config = require('./configuracion')
+//let mathjs = require('mathjs')
+var { atan2, chain, derivative, e, evaluate, log, pi, pow, round, sqrt } = require('mathjs')
+//var mathjs = require('mathjs')
+
 //Clases
 class Persona{
     id
     nombre
     apellido
-    fechaNacimiento
-    genero
 
-    constructor(id, nombre, apellido, fechaNac, gen){
+    constructor(id, nombre, apellido, fechaNacimiento){
         //logica de instanciación
         this.id = id
         this.nombre = nombre
         this.apellido = apellido
-        this.genero = gen
         this.cansancio = 0
-        this.fechaNacimiento = fechaNac
+        this.fechaNacimiento = fechaNacimiento
         this.fechaCreacion = new Date()
         this.informacionCompleta = `${this.nombre} - ${this.apellido} - Fecha de nacimiento: ${this.fechaNacimiento}` 
         this.calcularEdad()
@@ -24,12 +26,13 @@ class Persona{
     }
 
     caminar(kilometrosCaminados){
-        this.cansancio = this.cansancio + (1 * kilometrosCaminados)
+        this.cansancio = this.cansancio + (1.5 * kilometrosCaminados)
         console.log(`${this.nombre} ha caminado ${kilometrosCaminados} km., por lo que se encuentra con un nivel de cansancio ${this.cansancio}`)
     }
 
-    dormir(){
-
+    dormir(horasDeSueno){
+        this.cansancio = this.cansancio - (2 * horasDeSueno)
+        console.log(`${this.nombre} ha dormido ${horasDeSueno}, por lo que se encuentra con un nivel de cansancio ${this.cansancio}`)
     }
 
     hablar(){
@@ -37,24 +40,33 @@ class Persona{
     }
 }
 
-var persona1 = new Persona(1, "Luke", "Skywalker", 1974, "M", new Date(2005, 9, 24))
-var persona2 = new Persona(2, "Leia", "Skywalker", 1974, "F", new Date(2001, 0, 24))
-var persona3 = new Persona(3, "Obi", "wan kenobi", 1964, "M", new Date(2002, 3, 24))
-var persona4 = new Persona(4, "anakin", "skywalker", 1985, "M", new Date(2005, 9, 24))
-
 var persona1 = new Persona(1, "Luke", "Skywalker", new Date(2005, 9, 24)) // el mes es indexado como 0
 var persona2 = new Persona(2, "Leia", "Skywalker", new Date(2010, 0, 10))
 
-console.log(persona1.calcularEdad())
-persona1.fechaNacimiento = new Date(2002, 8, 27)
-console.log(persona1.calcularEdad())
-persona1.fechaNacimiento = new Date(2002, 10, 12)
-console.log(persona1.calcularEdad())
+
+//Edad
+//console.log(persona1.calcularEdad())
+//persona1.fechaNacimiento = new Date(2006, 9, 24)
+//console.log(persona1.calcularEdad())
+//persona1.fechaNacimiento = new Date(2010, 0, 10)
+//console.log(persona1.calcularEdad())
+
+//Cansancio
+console.log(persona1.cansancio)
+
+persona1.caminar(10)
+console.log(persona1.cansancio)
+
+persona1.dormir(8)
+console.log(persona1.cansancio)
+
+console.log(round(e, 3))
+//console.log(mathjs.log)
+
 //persona1.caminar(15)
 //console.log(persona1)
 //console.log(persona2)
 
-let = nombre = '';
 
 //Intentar calcular la edad a partir de la fecha de nacimiento
 
